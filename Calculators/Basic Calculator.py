76813419edc95de89c20b37af4023316ed2c1144
CodingWithMK <032180115@ogr.uludag.edu.tr>
2022-10-29 03:20:39 +0300

    My Very First Calculator

--- Calculators/Basic Calculator.py
+++ Calculators/Basic Calculator.py
@@ -1,5 +1,5 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = float(input("Enter the first number: ")) #'int' also can be used instead of 'float'
+num2 = float(input("Enter the second number: ")) #'int' also can be used instead of 'float'
 
 if num1 <= num2:
     print(num1 + num2)
