13e1d5ff1e59b6f96f1d7345498c3dd0c60eed82
CodingWithMK <032180115@ogr.uludag.edu.tr>
2024-01-03 17:25:38 +0300

    Updated with real password generating algorithm from a characteristics list.

--- PracticingProjects/password_generator.py
+++ PracticingProjects/password_generator.py
@@ -1,15 +1,18 @@
 import random
 
-def shuffle(string):
-    templist = list(string)
-    random.shuffle(templist)
-    return ''.join(templist)
+characteristics = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                   'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                   'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                   'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', 
+                   '9', '0', '@', '.', '!', '$', '_', '-', '?', '*', '#', '~']
 
-uppercaseLetter1 = chr(random.randint(65, 90))
-uppercaseLetter2 = chr(random.randint(65, 90))
-
-
-password = uppercaseLetter1 + uppercaseLetter2
-password = shuffle(password)
-
-print(password)
+password_length = int(input("Enter the length of your desired password. (between 8 and 16 characters): "))
+       
+def generate_password():
+    generated_password = ''.join(random.choice(characteristics) for _ in range(password_length))
+    return generated_password
+    
+    
+password = generate_password()
+print("Generated password is: ", password)
